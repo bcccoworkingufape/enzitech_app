d2ba022750cf0c71be24199dd3dd737af965e262
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2022-06-14 10:33:14 -0300

    Add bcccoworking logo to Account page

--- lib/src/features/home/fragments/account/account_page.dart
+++ lib/src/features/home/fragments/account/account_page.dart
@@ -1,10 +1,14 @@
 // 🐦 Flutter imports:
+import 'package:enzitech_app/src/shared/themes/app_complete_theme.dart';
+import 'package:enzitech_app/src/shared/util/util.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 // 📦 Package imports:
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:settings_ui/settings_ui.dart';
+import 'package:url_launcher/url_launcher.dart' as url_launcher;
 
 // 🌎 Project imports:
 import 'package:enzitech_app/src/features/home/fragments/account/account_controller.dart';
@@ -69,46 +73,74 @@ class _AccountPageState extends State<AccountPage> {
 
     final controller = context.watch<AccountController>();
 
-    return SizedBox(
-      height: heightMQ,
-      width: widthMQ,
-      child: SettingsList(
-        sections: [
-          SettingsSection(
-            title: const Text('Conta'),
-            tiles: <SettingsTile>[
-              SettingsTile(
-                leading: const Icon(PhosphorIcons.user),
-                title: const Text('Usuário'),
-                value: Text(
-                  controller.username ?? 'Não definido',
-                  style: customTextStyle,
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: SizedBox(
+        height: heightMQ,
+        width: widthMQ,
+        child: Column(
+          children: [
+            Expanded(
+              child: SettingsList(
+                lightTheme: const SettingsThemeData(
+                  settingsListBackground: AppColors.background,
                 ),
+                sections: [
+                  SettingsSection(
+                    title: const Text('Conta'),
+                    tiles: <SettingsTile>[
+                      SettingsTile(
+                        leading: const Icon(PhosphorIcons.user),
+                        title: const Text('Usuário'),
+                        value: Text(
+                          controller.username ?? 'Não definido',
+                          style: customTextStyle,
+                        ),
+                      ),
+                      SettingsTile(
+                        leading: const Icon(PhosphorIcons.at),
+                        title: const Text(
+                          'Email',
+                        ),
+                        value: Flexible(
+                          flex: 2,
+                          child: Text(
+                            controller.email ?? 'Não definido',
+                            overflow: TextOverflow.ellipsis,
+                            style: customTextStyle,
+                          ),
+                        ),
+                      ),
+                      SettingsTile.navigation(
+                        leading: const Icon(PhosphorIcons.signOut),
+                        title: const Text(
+                          'Sair',
+                        ),
+                        onPressed: (_) => controller.logout(),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-              SettingsTile(
-                leading: const Icon(PhosphorIcons.at),
-                title: const Text(
-                  'Email',
-                ),
-                value: Flexible(
-                  flex: 2,
-                  child: Text(
-                    controller.email ?? 'Não definido',
-                    overflow: TextOverflow.ellipsis,
-                    style: customTextStyle,
+            ),
+            GestureDetector(
+              onTap: () => url_launcher.launchUrl(
+                Uri.parse(Constants.bccCoworkingLink),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SvgPicture.asset(
+                    AppSvgs.developedBy,
+                    alignment: Alignment.bottomCenter,
+                    width: MediaQuery.of(context).size.width,
                   ),
                 ),
               ),
-              SettingsTile.navigation(
-                leading: const Icon(PhosphorIcons.signOut),
-                title: const Text(
-                  'Sair',
-                ),
-                onPressed: (_) => controller.logout(),
-              ),
-            ],
-          ),
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/shared/themes/app_svgs.dart
+++ lib/src/shared/themes/app_svgs.dart
@@ -1,4 +1,5 @@
 class AppSvgs {
   static const fullLogo = "assets/images/full_logo.svg";
   static const iconLogo = "assets/images/icon_logo.svg";
+  static const developedBy = "assets/images/developed_by.svg";
 }
