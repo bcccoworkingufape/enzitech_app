c5748fd6c2e3c293618510efba2fe99fd41953bd
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2022-10-06 16:19:14 -0300

    Configure iOS Firebase

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,18 @@
 import UIKit
 import Flutter
+import FirebaseCore
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+  
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
