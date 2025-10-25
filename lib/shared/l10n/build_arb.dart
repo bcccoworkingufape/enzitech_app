746b20438ee09d5ed56fe5860d900afa81cb2e6b
Armstrong Lohãns <lohansdemelo1108@gmail.com>
2025-10-24 22:47:00 -0300

    mock arb

--- lib/shared/l10n/build_arb.dart
+++ lib/shared/l10n/build_arb.dart
@@ -6,29 +6,32 @@ import 'dart:io';
 Future<void> main() async {
   final locales = ['en', 'pt'];
 
-  for (final locale in locales) {
-    final directory = Directory('lib/shared/l10n/$locale');
-    final outputFile = File('lib/shared/l10n/app_$locale.arb');
+  try {
+    for (final locale in locales) {
+      final directory = Directory('lib/shared/l10n/$locale');
+      final outputFile = File('lib/shared/l10n/app_$locale.arb');
 
-    final Map<String, dynamic> merged = {"@@locale": locale};
+      final Map<String, dynamic> merged = {"@@locale": locale};
 
-    if (!await directory.exists()) continue;
+      if (!await directory.exists()) continue;
 
-    final files = directory.listSync().whereType<File>().where((f) => f.path.endsWith('.arb')).toList();
+      final files = directory.listSync().whereType<File>().where((f) => f.path.endsWith('.arb')).toList();
 
-    for (final file in files) {
-      final content = await file.readAsString();
-      final jsonMap = json.decode(content) as Map<String, dynamic>;
+      for (final file in files) {
+        final content = await file.readAsString();
+        final jsonMap = json.decode(content) as Map<String, dynamic>;
 
-      merged.addAll(jsonMap);
+        merged.addAll(jsonMap);
+      }
+
+      // Grava o arquivo final formatado
+      final encoder = JsonEncoder.withIndent('  ');
+      await outputFile.writeAsString(encoder.convert(merged));
+
+      print('✅ Gerado: ${outputFile.path}');
     }
-
-    // Grava o arquivo final formatado
-    final encoder = JsonEncoder.withIndent('  ');
-    await outputFile.writeAsString(encoder.convert(merged));
-
-    print('✅ Gerado: ${outputFile.path}');
+    print('\n🎉 Todos os arquivos ARB foram mesclados com sucesso!');
+  } catch (e) {
+    print('Erro ao gerar os arquivos: $e');
   }
-
-  print('\n🎉 Todos os arquivos ARB foram mesclados com sucesso!');
 }
