39c569c413bf6bcc5f79c9e80d228a88f28ebc82
Guilherme Felix <guilherme.flima@ufape.edu.br>
2026-08-01 16:46:53 -0300

    feat: create treatments inline during experiment creation

--- lib/features/treatment/data/datasources/treatments_datasource.dart
+++ lib/features/treatment/data/datasources/treatments_datasource.dart
@@ -8,7 +8,7 @@ import '../../domain/entities/treatment_entity.dart';
 abstract class TreatmentsDataSource {
   Future<Either<Failure, Unit>> deleteTreatment(String id);
 
-  Future<Either<Failure, Unit>> createTreatment({required String name, required String description});
+  Future<Either<Failure, TreatmentEntity>> createTreatment({required String name, required String description});
 
   Future<Either<Failure, List<TreatmentEntity>>> getTreatments();
 }
