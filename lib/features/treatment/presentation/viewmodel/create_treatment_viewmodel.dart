ff9eed88e936a8bb1d506e46752d0e3fd9f53e36
Guilherme Felix <guilherme.flima@ufape.edu.br>
2026-08-01 16:47:34 -0300

    refactor: simplify treatment creation success callback

--- lib/features/treatment/presentation/viewmodel/create_treatment_viewmodel.dart
+++ lib/features/treatment/presentation/viewmodel/create_treatment_viewmodel.dart
@@ -34,7 +34,7 @@ class CreateTreatmentViewmodel extends ChangeNotifier {
         _setFailure(error);
         setStateEnum(StateEnum.error);
       },
-      (success) async {
+      (success) {
         setStateEnum(StateEnum.success);
       },
     );
